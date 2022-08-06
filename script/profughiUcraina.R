52da695a0b902d2c06d4b31972f49eac90996e5b
vi-enne <vittorio.nicoletta.1@ulaval.ca>
2022-08-06 14:14:14 -0400

    Update profughiUcraina.R

--- script/profughiUcraina.R
+++ script/profughiUcraina.R
@@ -13,55 +13,63 @@ token <- create_token(
 
 # Download tweets
 tweet_df <- search_tweets("#accoglienzaucraina sono from:viminale",
-                          include_rts = FALSE,
-                          token = token)
+                          include_rts = FALSE)
 
-temp <- as.data.frame(tweet_df[1:5])
-temp$created_at <- as.character(temp$created_at)
+nTweet <- nrow(tweet_df)
 
-
-# # Load previous version
-# if(file.exists(here("data", "tweet.csv"))){
-#   hist <- read.csv(here("data", "tweet.csv"), colClasses = "character")
-#   temp <- temp[!(temp$status_id %in% hist$status_id),]
-#   hist <- rbind(temp, hist)
-# }else{
-#   hist <- temp
-# }
-#
-# n <- nrow(temp)
-
-# If there is a new tweet, extract information and save files
-# if(n>0){
-
-n <- nrow(temp)
-
-temp$text <- gsub("\\.", "", temp$text)
-
-res <- data.frame(matrix(NA, nrow = n, ncol = 5))
-colnames(res) <- c("Data", "Totale", "Uomini", "Donne", "Minori")
-
-for (i in 1:n) {
-  res$Data[i]   <- temp$created_at[i]
-  res$Totale[i] <- as.numeric(str_extract(temp$text[i], "[0-9]+(?=\\s*le)"))
-  res$Donne[i]  <- as.numeric(str_extract(temp$text[i], "[0-9]+(?=\\s*donne)"))
-  res$Uomini[i] <- as.numeric(str_extract(temp$text[i], "[0-9]+(?=\\s*uomini)"))
-  res$Minori[i] <- as.numeric(str_extract(temp$text[i], "[0-9]+(?=\\s*minori)"))
+if (nTweet > 0) {
+  temp <- as.data.frame(tweet_df[1:5])
+  temp$created_at <- as.character(temp$created_at)
+  
+  
+  # # Load previous version
+  # if(file.exists(here("data", "tweet.csv"))){
+  #   hist <- read.csv(here("data", "tweet.csv"), colClasses = "character")
+  #   temp <- temp[!(temp$status_id %in% hist$status_id),]
+  #   hist <- rbind(temp, hist)
+  # }else{
+  #   hist <- temp
+  # }
+  #
+  # n <- nrow(temp)
+  
+  # If there is a new tweet, extract information and save files
+  # if(n>0){
+  
+  n <- nrow(temp)
+  
+  temp$text <- gsub("\\.", "", temp$text)
+  
+  res <- data.frame(matrix(NA, nrow = n, ncol = 5))
+  colnames(res) <- c("Data", "Totale", "Uomini", "Donne", "Minori")
+  
+  for (i in 1:n) {
+    res$Data[i]   <- temp$created_at[i]
+    res$Totale[i] <-
+      as.numeric(str_extract(temp$text[i], "[0-9]+(?=\\s*le)"))
+    res$Donne[i]  <-
+      as.numeric(str_extract(temp$text[i], "[0-9]+(?=\\s*donne)"))
+    res$Uomini[i] <-
+      as.numeric(str_extract(temp$text[i], "[0-9]+(?=\\s*uomini)"))
+    res$Minori[i] <-
+      as.numeric(str_extract(temp$text[i], "[0-9]+(?=\\s*minori)"))
+  }
+  
+  res$Data <- as.character(as.Date(res$Data))
+  
+  if (file.exists(here("data", "outputProfughi.csv"))) {
+    hist_res <- read.csv(here("data", "outputProfughi.csv"))
+    # res <- rbind(res, hist_res)
+    res <- unique(rbind(res, hist_res))
+  }
+  
+  write.csv(
+    res,
+    file = here("data", "outputProfughi.csv"),
+    row.names = F,
+    fileEncoding = "UTF-8"
+  )
+  # write.csv(hist, file = here("data", "tweet.csv"), row.names = F, fileEncoding = "UTF-8")
+  # }
+  
 }
-
-res$Data <- as.character(as.Date(res$Data))
-
-if (file.exists(here("data", "outputProfughi.csv"))) {
-  hist_res <- read.csv(here("data", "outputProfughi.csv"))
-  # res <- rbind(res, hist_res)
-  res <- unique(rbind(res, hist_res))
-}
-
-write.csv(
-  res,
-  file = here("data", "outputProfughi.csv"),
-  row.names = F,
-  fileEncoding = "UTF-8"
-)
-# write.csv(hist, file = here("data", "tweet.csv"), row.names = F, fileEncoding = "UTF-8")
-# }
